1d4c26f5c6ff489626a61778b8e65b06c5935721
Sergio Aracil <sergi_a01@hotmail.com>
2016-07-18 10:03:05 +0200

    Add files via upload

---
+++ LenguajeCProfe/holamundo.c
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include<stdio.h>
+int main5()
+{
+    printf("Hola mundo!!\n\n");
+    return 0;
+}
+
+    
