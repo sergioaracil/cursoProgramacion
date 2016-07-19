ee435113d38cd249b792d94a5f66aa2b02501ace
Bernardo <>
2016-07-19 12:07:49 +0200

    Modificado forma de convertir a Romano y utiliza la funcion leerNumero modificada

--- SolNumRomanos-vectores.c
+++ SolNumRomanos-vectores.c
@@ -1,33 +1,54 @@
 /*Se crea funcion llamada leerNumero para que lea el numero en forma de texto y lo valide*/
-
 int leerNumero();
 
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
+    int r,digito;
     
-    
-    
-    int N;
-    const char *Unidad[]={"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    const char *Decena[]={"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    const char *Centena[]= {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    
-    
-    N=leerNumero();
-    int u=N%10;
-    int d=(N/10)%10;
-    int c=N/100;
+r=leerNumero();
+printf("\nEl numero %d en ROMANOS es ",r);
+digito=r/1000;
+r=r%1000;
+cambiarRom(digito,'M',' ',' ');
+digito=r/100;
+r=r%100;
+cambiarRom(digito,'C','D','M');
+digito=r/10;
+r=r%10;
+cambiarRom(digito,'X','L','C');
+digito=r;
+cambiarRom(digito,'I','V','X');
 
-      if(N>=100){
-          printf("%s%s%s",Centena[c],Decena[d],Unidad[u]);
-      }else{
-          if(N>=10){
-              printf("%s%s",Decena[d],Unidad[u]);
-          }else{
-              printf("%s",Unidad[N]);
-          }
-      }
-    return 0;
+printf("\n\n");
+       
+return (0);
+}
+
+void cambiarRom(int digito, char v1,char v2,char v3)
+{
+int j;
+
+if(digito==9){
+printf("%c%c",v1,v3);
+}
+else{
+if(digito>4){
+printf("%c",v2);
+for(j=1;j<=digito-5;j++){
+printf("%c",v1);
+}
+}
+else{
+if(digito==4){
+printf("%c%c",v1,v2);
+}
+else{
+for(j=1;j<=digito;j++){
+printf("%c",v1);
+}
+}
+}
+}
 }
