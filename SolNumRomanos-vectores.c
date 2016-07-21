7e10503c86ec340fe89094b8f514ffb4c862d0b3
Angel <Programacion@PC-6>
2016-07-21 09:41:34 +0200

    Ejercicio resuelto

--- SolNumRomanos-vectores.c
+++ SolNumRomanos-vectores.c
@@ -1,23 +1,28 @@
+/*Se crea funcion llamada leerNumero para que lea el numero en forma de texto y lo valide*/
+
 int leerNumero();
 
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
-    
-    
-    
+        
     int N;
     const char *Unidad[]={"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
     const char *Decena[]={"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
     const char *Centena[]= {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    
+    const char *Millar[]={"","M","MM","MMM"};
     
     N=leerNumero();
     int u=N%10;
     int d=(N/10)%10;
-    int c=N/100;
+    int c=(N/100)%10;
+    int m=N/1000;
 
+ 
+    if (N>=1000){
+        printf("%s%s%s%s",Millar[m],Centena[c],Decena[d],Unidad[u]);
+    }else{
       if(N>=100){
           printf("%s%s%s",Centena[c],Decena[d],Unidad[u]);
       }else{
@@ -27,5 +32,6 @@ int main()
               printf("%s",Unidad[N]);
           }
       }
+    } 
     return 0;
 }
--- leerNumero.c
+++ leerNumero.c
@@ -1,21 +1,25 @@
-
+#include<stdio.h>
+#include<stdlib.h>
 int leerNumero()
 {
     
     int num;
     char stringNum[5];
-    
+  do {  
     printf("Ingrese numero entre 1 y 3500: \n");
     scanf("%s",stringNum);
     num=atoi(stringNum); /* atoi convierte el numero ingresado como cadena en entero
     en caso de ser posible, de lo contrario retorna 0*/
     
+/*Comprobar que se ha convertido correctamente en un numero entero*/
     if(num!=0) 
         printf("El numero es: %d\n",num);
     else
         printf("No es un numero\n");   
     
     
+   // return num;
+  }
+    while ((num == 0) || (num>3500));
     return num;
-    
 }
